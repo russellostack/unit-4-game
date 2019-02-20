a6b06c2629c6728f2c1c724d7d7ff557bb686057
russellostack <32462495+russellostack@users.noreply.github.com>
2019-02-20 16:26:07 -0500

    html done, js game objects started. still need functions, css

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+var heros = [maul={
+    name:"Darth Maul",
+    hp:100,
+    attack=(Math.floor(Math.random()*10)+5),
+    counter=(Math.floor(Math.random()*10)+10),
+    isPlayerOne=false,
+    isPlayerTwo=false
+},vader={
+    name:"Darth Vader",
+    hp:180,
+    attack=(Math.floor(Math.random()*10)+10),
+    counter=(Math.floor(Math.random()*10)+1),
+    isPlayerOne=false,
+    isPlayerTwo=false
+},luke={
+    name:"Luke",
+    hp:160,
+    attack=(Math.floor(Math.random()*10)+7),
+    counter=(Math.floor(Math.random()*10)+4),
+    isPlayerOne=false,
+    isPlayerTwo=false
+},yoda={
+    name:"Yoda",
+    hp:200,
+    attack=(Math.floor(Math.random()*10)+8),
+    counter=(Math.floor(Math.random()*10)+12),
+    isPlayerOne=false,
+    isPlayerTwo=false
+}]
