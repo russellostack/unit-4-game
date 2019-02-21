d5e20af0f243fbac39214adc7e9dde7fc78ff494
russellostack <32462495+russellostack@users.noreply.github.com>
2019-02-21 17:16:37 -0500

    most of the js is in, still need winning and losing functions. need to do all of the css still.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -27,3 +27,109 @@ var heros = [maul={
     isPlayerOne=false,
     isPlayerTwo=false
 }]
+var player;
+var enemy;
+playerOneSelected = false;
+playerTwoSelected = false;
+gameStart = false;
+var enemyHp;
+var enemiesRemaining = players.length -1;
+
+function playerSelect(){
+    $("#maul").on("click", function(){
+        if (playerOneSelected === false){
+            $("#maul").appendTo("#fightArea");
+            $("#hero-name").text(maul.name);
+            $("#hero-hp").text("Health Points");
+			playerOneSelected = true;
+			maul.isPlayerOne = true;
+			player = rey;
+			enemySelect();
+        }
+    });
+    $("#vader").on("click", function(){
+        if (playerOneSelected === false){
+            $("#vader").appendTo("#fightArea");
+            $("#hero-name").text(vader.name);
+            $("#hero-hp").text("Health Points");
+			playerOneSelected = true;
+			vader.isPlayerOne = true;
+			player = vader;
+			enemySelect();
+        }
+    });
+    $("#luke").on("click", function(){
+        if (playerOneSelected === false){
+            $("#luke").appendTo("#fightArea");
+            $("#hero-name").text(luke.name);
+            $("#hero-hp").text("Health Points");
+			playerOneSelected = true;
+			luke.isPlayerOne = true;
+			player = luke;
+			enemySelect();
+        }
+    });
+    $("#yoda").on("click", function(){
+        if (playerOneSelected === false){
+            $("#yoda").appendTo("#fightArea");
+            $("#hero-name").text(yoda.name);
+            $("#hero-hp").text("Health Points");
+			playerOneSelected = true;
+			yoda.isPlayerOne = true;
+			player = yoda;
+			enemySelect();
+        }
+    });
+
+}
+function enemySelect(){
+
+}
+
+function fight(){
+    $("#attackBtn").on("click", function() {
+        if (gameStart === true){
+            attack();
+            if (enemy.enemyHp <= 0){
+            enemiesRemaining--;
+            statusCheck();
+            } else {
+            counterAttack();
+            }
+            
+        }
+        
+    });		
+}
+function attack(){
+    enemy.enemyHp = enemy.enemyHp - player.attack;
+    $("#enemy-hp").text(enemy.enemyHp);
+}
+function counterAttack(){
+    player.hp = player.hp - enemy.counter;
+    $("#hero-hp").text(player.hp);
+    if (player.hp <= 0){
+        gameLose();	
+    }
+}
+function statusCheck(){
+    if (enemiesRemaining === 0){
+    gameWin();
+    }
+    else {	
+        gameStart = false;
+            if (enemiesRemaining === 2){
+            } else if (enemiesRemaining === 1){
+    }
+        $("#enemy-fight-area").empty();
+        $("#enemy-name").text("");
+        $("#enemy-total-hp").text("");
+        $('#enemy-hp').text("");
+    }	
+}
+function gameLose(){
+
+}
+function gameWin(){
+    
+}
